9b5367a88842e016f8ae1e0dd56c86dc2e9896ad
Zeztron <pateltharsh@gmail.com>
2019-06-02 13:32:56 -0400

    added grid

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import Welcome from "./WelcomeMessage"
+import Welcome from "./WelcomeMessage";
+import AppLayout from './AppLayout';
+import AppBar from './AppBar';
 
 class App extends Component {
   render() {
     return (
-      <Welcome/>
+      <AppLayout>
+        <AppBar/>
+        <Welcome />
+      </AppLayout>
     );
   }
 }
